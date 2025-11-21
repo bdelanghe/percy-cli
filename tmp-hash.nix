6ebeec6e89917827975b974179532075511d36a6
Robert DeLanghe <1240090+bdelanghe@users.noreply.github.com>
2025-11-21 17:11:47 -0500

    Update tmp-hash.nix to use a specific Nixpkgs flake for dependency management on aarch64-darwin. This change enhances consistency in package fetching by referencing a fixed commit hash.

--- tmp-hash.nix
+++ tmp-hash.nix
@@ -1,6 +1,7 @@
 let
   flake = builtins.getFlake (toString ./.);
-  pkgs = import <nixpkgs> { system = "aarch64-darwin"; };
+  nixpkgs = builtins.getFlake "github:NixOS/nixpkgs/b134951a4c9f3c995fd7be05f3243f8ecd65d798";
+  pkgs = import nixpkgs { system = "aarch64-darwin"; };
 in
 pkgs.fetchYarnDeps {
   yarnLock = ./yarn.lock;
